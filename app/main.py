2066c05c7d1a2cba033c5bcff929f4130e5dc729
mohammedsabeel <mohammedsabeeltc786@gmail.com>
2024-07-07 18:09:31 +0530

    email api added

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI,Depends,HTTPException
 from typing import List,Annotated
 from sqlalchemy.orm import Session
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 from app.database import engine,get_db
 from .schemas import UserCreate,UserResponse,UserUpdate
@@ -46,4 +49,68 @@ def delete_user(user_id: int, db:db_dependency):
     db.delete(db_user)
     db.commit()
     return {"message": "User deleted"}
+
+
+
+@app.post("/send_invite", tags=["Send mail"])
+def send_invite():
+    sender_email = "mohammedsabeeltc786@gmail.com"
+    recipients = ["sabeelmohammedtc786@gmail.com"]
+    # recipients = ["shraddha@aviato.consulting", "pooja@aviato.consulting"]
+    subject = "API Documentation Invitation"
+    body = """
+    <html>
+    <body>
+        <div
+            style="width: 600px; margin: auto; padding: 20px; font-family: Arial, sans-serif; background-color: #f4f4f4; border: 1px solid #ddd;">
+            <h1
+                style="background-color: #3971e0; color: white; padding: 10px 0; text-align: center;">API
+                Documentation Invitation</h1>
+            <p>Hello,</p>
+            <p>I have excited to invite you to view my User Management API
+                documentation on <strong>ReDoc</strong>.</p>
+            <p>You can access the documentation by clicking the button
+                below:</p>
+            <a href="https://crud-428709.el.r.appspot.com/redoc"
+                style="display: inline-block; padding: 10px 20px; color: white; background-color: #3971e0; text-decoration: none; border-radius: 5px;">View
+                API Documentation</a>
+            <p>As per the requirements, I have set up the API to handle user
+                management for three different projects. The API supports the
+                following operations:</p>
+            <ul>
+                <li>Api For Creating User</li>
+                <li>Api Listing User Details</li>
+                <li>Api For Updating User Details</li>
+                <li>Api For Deleting User</li>
+            </ul>
+            <p>I have also set up a GCP free tier account for deployment and GCP
+                Postgres for the database.</p>
+            <p>I have appreciate your time and look forward to your
+                feedback.</p>
+            <div
+                style="width: 500px;background-color: #3971e0; margin: auto; padding: 20px; font-family: Arial, sans-serif; backgro border: 1px solid #ddd; color:white;text-align:center;">
+                <p style="font-size:13px;">Thank you,</p>
+                <p style="font-size:12px;">Mohammed Sabeel Tc</p>
+                <p style=" font-size: 12px;">If you have any questions, feel
+                    free to reply to this <a href="mailto:mohammedsabeeltc786@gmail.com">mohammedsabeeltc786@gmail.com</a>.</p>
+            </div>
+        </div>
+    </body>
+</html>
+    """
+
+    msg = MIMEMultipart("alternative")
+    msg["From"] = sender_email
+    msg["To"] = ", ".join(recipients)
+    msg["Subject"] = subject
+    msg.attach(MIMEText(body, "html"))
+
+    try:
+        with smtplib.SMTP("smtp.gmail.com", 587) as server:
+            server.starttls()
+            server.login(sender_email, "pijwwvkyyxmhdccr")
+            server.sendmail(sender_email, recipients, msg.as_string())
+        return {"message": "Invitation email sent successfully"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Failed to send email: {e}")
 
